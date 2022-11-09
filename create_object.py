ac85e7d23cb118d663a2b319b90d14e7f124b4e1
TTA-Dev-Alex <114209303+TTA-Dev-Alex@users.noreply.github.com>
2022-11-09 09:42:50 +0700

    Update create_object.py

--- create_object.py
+++ create_object.py
@@ -4,4 +4,8 @@ bpy.ops.mesh.primitive_cube_add()
 #this line will create a cylinder
 bpy.ops.mesh.primitive_cylinder_add()
 
-#
+#this line will create a cone
+bpy.ops.mesh.primitive_cone_add()
+
+#this line will create a sphere
+bpy.ops.mesh.primitive_cone_add()
